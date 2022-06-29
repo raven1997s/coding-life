caf7c0278bb498f39ab1dc8a9ddf014e1f9be90a
gaoning <gaoning@1152522707067778.onaliyun.com>
2022-06-29 16:52:34 +0800

    后序遍历-迭代方式 通过栈的特效 先进后出，控制遍历元素的顺序。

---
+++ spring/src/main/java/com/raven/algorithm/exercise/二叉树/_145_二叉树的后序遍历.java
@@ -0,0 +1,62 @@
+package com.raven.algorithm.exercise.二叉树;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Description:
+ * date: 2022/6/29 15:39
+ * https://leetcode.cn/problems/binary-tree-postorder-traversal/
+ *
+ * @author raven
+ */
+public class _145_二叉树的后序遍历 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        postorderTraversal(root, result);
+        return result;
+    }
+
+    /**
+     * 递归方式
+     *
+     * @param node
+     * @param result
+     */
+    private void postorderTraversal(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        postorderTraversal(node.left, result);
+        postorderTraversal(node.right, result);
+        result.add(node.val);
+    }
+
+    /**
+     * 迭代方式
+     *
+     * @param root
+     * @return
+     */
+    private void postorderTraversal1(TreeNode root, LinkedList<Integer> result) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        while (!stack.isEmpty() || root != null) {
+            if (root != null) {
+                stack.push(root);
+                result.addFirst(root.val);
+                root = root.right;
+            } else {
+                root = stack.pop();
+                root = root.left;
+            }
+        }
+    }
+}
