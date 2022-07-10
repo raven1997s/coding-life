d9d945c0758fa36737ba3105f3e22901cb3e44eb
gaoning <gaoning@1152522707067778.onaliyun.com>
2022-07-10 22:38:27 +0800

    _98_验证二叉搜索树.java _108_将有序数组转换为二叉搜索树.java _173_二叉搜索树迭代器.java _230_二叉搜索树中第K小的元素.java _235_二叉搜索树的最近公共祖先.java _450_删除二叉搜索树中的节点.java _530_二叉搜索树的最小绝对差.java _700_二叉搜索树中的搜索.java _701_二叉搜索树中的插入操作.java _783_二叉搜索树节点最小距离.java _938_二叉搜索树的范围和.java

---
+++ spring/src/main/java/com/raven/algorithm/exercise/二叉搜索树/_173_二叉搜索树迭代器.java
@@ -0,0 +1,23 @@
+package com.raven.algorithm.exercise.二叉搜索树;
+
+/**
+ * Description:
+ * date: 2022/7/10 22:26
+ * https://leetcode.cn/problems/binary-search-tree-iterator/
+ *
+ * @author raven
+ */
+public class _173_二叉搜索树迭代器 {
+
+    public _173_二叉搜索树迭代器(TreeNode root) {
+
+    }
+
+    public int next() {
+        return 0;
+    }
+
+    public boolean hasNext() {
+        return false;
+    }
+}
---
+++ spring/src/main/java/com/raven/algorithm/exercise/二叉搜索树/_98_验证二叉搜索树.java
@@ -0,0 +1,39 @@
+package com.raven.algorithm.exercise.二叉搜索树;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Description:
+ * date: 2022/7/10 17:47
+ * https://leetcode.cn/problems/validate-binary-search-tree/
+ *
+ * @author raven
+ */
+public class _98_验证二叉搜索树 {
+    private LinkedList<TreeNode> list = new LinkedList<>();
+
+    /**
+     * 利用中序遍历将元素排序，排序后如果前一个元素大于等于后一个元素，则不是二叉搜索树
+     * @param root
+     * @return
+     */
+    public boolean isValidBST(TreeNode root) {
+        inOrder(root);
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).val >= list.get(i + 1).val) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void inOrder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        list.add(node);
+        inOrder(node.right);
+    }
+}
