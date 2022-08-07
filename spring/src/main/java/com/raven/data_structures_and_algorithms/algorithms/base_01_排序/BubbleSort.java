d03021b44e54b31b189201fa2efbbd6a1f8f95e2
raven <gaoning176@gmail.com>
2022-08-07 17:45:40 +0800

    冒泡排序 优化2 如果数组已经部分有序，则记录有序的位置，减少循环比较有序的数据

--- spring/src/main/java/com/raven/data_structures_and_algorithms/algorithms/base_01_排序/BubbleSort.java
+++ spring/src/main/java/com/raven/data_structures_and_algorithms/algorithms/base_01_排序/BubbleSort.java
@@ -20,11 +20,13 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         //Integer[] ints = Integers.random(10000, 1, 100000);
-        Integer[] ints = Integers.ascOrder(1, 10000);
+        Integer[] ints = Integers.tailAscOrder(1, 10000,2000);
         Integer[] ints2 = Integers.copy(ints);
+        Integer[] ints3 = Integers.copy(ints);
 
         Times.test("sort1", () -> sort(ints));
         Times.test("sort2", () -> sort2(ints2));
+        Times.test("sort3", () -> sort3(ints3));
     }
 
     /**
@@ -52,7 +54,7 @@ public class BubbleSort {
 
     /**
      * 冒泡排序
-     * 优化：如果数组已经有序，则提前结束循环
+     * 优化：如果数组已经全部有序，则提前结束循环
      *
      * @param ints
      */
@@ -80,6 +82,38 @@ public class BubbleSort {
             if (sorted) {
                 break;
             }
+        }
+    }
+
+
+    /**
+     * 冒泡排序
+     * 优化：如果数组已经部分有序，则记录有序的位置，减少循环比较有序的数据
+     *
+     * @param ints
+     */
+    static void sort3(Integer[] ints) {
+
+        // 外循环控制比较的次数
+        // 内循环是比较交换元素 已经有序的元素不再比较
+        // 内循环每次遍历后会将最大的元素放到最后，所以外循环每次需要对比的元素都少一个
+        for (int end = ints.length - 1; end > 0; end--) {
+            // 默认一开始所有元素就全部有顺序
+            int sortedIndex = 1;
+            // 从第二个元素开始和前一个元素俩俩比较，如果当前元素比前一个元素小就交换位置
+            // 已经有序的元素不再比较
+            for (int begin = 1; begin <= end; begin++) {
+                if (ints[begin] < ints[begin - 1]) {
+                    int temp = ints[begin];
+                    ints[begin] = ints[begin - 1];
+                    ints[begin - 1] = temp;
+                    // 记录进行了比较的index位置
+                    sortedIndex = begin;
+                }
+            }
+            // 设置目前已经有序的索引位置，下次外循环直接比较到该位置即可
+            end = sortedIndex;
+
         }
     }
 }
