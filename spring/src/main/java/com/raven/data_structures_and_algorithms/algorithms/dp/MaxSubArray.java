8445b9416b30e9967252e757bffeaa8946e9030b
raven <gaoning176@gmail.com>
2022-11-16 10:56:21 +0800

    动态规划 最大连续子序列和

--- spring/src/main/java/com/raven/data_structures_and_algorithms/algorithms/dp/MaxSubArray.java
+++ spring/src/main/java/com/raven/data_structures_and_algorithms/algorithms/dp/MaxSubArray.java
@@ -9,9 +9,15 @@ package com.raven.data_structures_and_algorithms.algorithms.dp;
  */
 public class MaxSubArray {
     public static void main(String[] args) {
-        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(maxSubArray2(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 
+    /**
+     * // 定义dp数组，dp[i] 以nums[i] 结尾的最大连续子序列和
+     *
+     * @param nums
+     * @return
+     */
     static int maxSubArray(int[] nums) {
         if (nums == null || nums.length == 0) {
             return -1;
@@ -32,6 +38,28 @@ public class MaxSubArray {
             }
             System.out.println("dp[" + i + "] = " + dp[i]);
             max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+
+    /**
+     * 优化空间
+     *
+     * @param nums
+     * @return
+     */
+    static int maxSubArray2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int dp = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // 如果dp[i - 1] 小于 0 那么dp[i] 的连续子序列和不需要加上dp[i - 1]
+            dp = dp < 0 ? nums[i] : dp + nums[i];
+            max = Math.max(max, dp);
         }
         return max;
     }
