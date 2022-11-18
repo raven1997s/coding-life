1eb25122573228643bee4cd261baae0b1507ac91
raven <gaoning176@gmail.com>
2022-11-18 11:07:40 +0800

    动态规划-优化

--- spring/src/main/java/com/raven/data_structures_and_algorithms/algorithms/dp/LCSubstring.java
+++ spring/src/main/java/com/raven/data_structures_and_algorithms/algorithms/dp/LCSubstring.java
@@ -11,10 +11,52 @@ import org.apache.commons.lang3.StringUtils;
 public class LCSubstring {
 
     public static void main(String[] args) {
-        System.out.println(lcs("CABCD", "FEEG"));
+        System.out.println(lcs2("CABCD", "FEEGD"));
     }
 
 
+    /**
+     * 倒叙遍历 优化调用参数leftTop
+     * @param str1
+     * @param str2
+     * @return
+     */
+    static int lcs2(String str1, String str2) {
+        if (StringUtils.isEmpty(str1) || StringUtils.isEmpty(str2)) {
+            return 0;
+        }
+        char[] chars1 = str1.toCharArray();
+        char[] chars2 = str2.toCharArray();
+        // 假设 dp(i, j) 是以str1[i-1] str2[j-1]结尾的最长公共子串长度
+
+        char[] rowChars = chars1;
+        char[] colsChars = chars2;
+        if (chars1.length < chars2.length) {
+            colsChars = chars1;
+            rowChars = chars2;
+        }
+        int[] dp = new int[colsChars.length + 1];
+        int max = 0;
+        for (int row = 1; row <= rowChars.length; row++) {
+            for (int col = colsChars.length; col > 1; col--) {
+                if (chars1[row - 1] != chars2[col - 1]) {
+                    dp[col] = 0;
+                } else {
+                    dp[col] = dp[col - 1] + 1;
+                    max = Math.max(max, dp[col]);
+                }
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 一维数组优化
+     *
+     * @param str1
+     * @param str2
+     * @return
+     */
     static int lcs(String str1, String str2) {
         if (StringUtils.isEmpty(str1) || StringUtils.isEmpty(str2)) {
             return 0;
@@ -44,7 +86,6 @@ public class LCSubstring {
                 }
             }
         }
-
         return max;
     }
 
